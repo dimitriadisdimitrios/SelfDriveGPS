83aa90eddf14ddb8276b57453945779bcecd5b34
Dimitriadis983 <dimitris.dimtriadis@gmail.com>
2018-06-06 11:54:13 +0300

    UnComment ArrayList with LatLog to continue with next milestone

--- app/src/main/java/gr/teicm/informatics/selfdrivegps/RetrieveDataActivity.java
+++ app/src/main/java/gr/teicm/informatics/selfdrivegps/RetrieveDataActivity.java
@@ -3,12 +3,14 @@ package gr.teicm.informatics.selfdrivegps;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -16,6 +18,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class RetrieveDataActivity extends Activity {
     final String TAG = "RetrieveDataActivity";
@@ -33,15 +36,31 @@ public class RetrieveDataActivity extends Activity {
         mRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                //Get child names from FireBase to show it on ListView
                 for(DataSnapshot child : dataSnapshot.getChildren()) {
                     String id = child.getKey();
-                    if(id!=null){
-                        fList.add(id);
-                    }
+                    fList.add(id);
                 }
+                //TODO: Check warning!!!
+                //Retrieve data from fireBase and place it on Map and then ArrayLis
+                Map<String, LatLng> baseRetrievedData = (Map<String, LatLng>) dataSnapshot.getValue();
+                ArrayList<LatLng> listOfLatLng = new ArrayList<>();
+                if(baseRetrievedData != null) {
+                    listOfLatLng.addAll(baseRetrievedData.values());
+                }
+                else{
+                    Log.d(TAG, "Attempt to invoke interface method 'java.util.Collection java.util.Map.values()' on a null object reference");
+                }
+                for(int i=0;i<listOfLatLng.size();i++)
+                {
+                    Log.d(TAG, String.valueOf(listOfLatLng.get(i)));
+                }
+
+                //Create ListView to show data from FireBase
                 ListView listView = (ListView) findViewById(R.id.list_view_main_frame);
                 ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.list_view, R.id.list_view_sample, fList);
                 listView.setAdapter(adapter);
+                //When you click Items on ListView it send you to maps Activity
                 listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -57,29 +76,5 @@ public class RetrieveDataActivity extends Activity {
                 System.out.println("The read failed: " + databaseError.getCode());
             }
         });
-
-//        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
-//        rootRef.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                //Retrieve data from fireBase and place it on Map and then ArrayLis
-//                //TODO: Check warning!!!
-//        //TODO: Retrive Lat-Log from FireBase and place it on ArrayList<LatLng>
-//                Map<String, LatLng> baseRetrievedData = (Map<String, LatLng>) dataSnapshot.getValue();
-//                ArrayList<LatLng> listOfLatLng = new ArrayList<>();
-//
-//                if(baseRetrievedData != null) {
-//                    listOfLatLng.addAll(baseRetrievedData.values());
-//
-//                }
-//                else{
-//                    Log.d(TAG, "Attempt to invoke interface method 'java.util.Collection java.util.Map.values()' on a null object reference");
-//                }
-//
-    }
-    @Override
-    public void onBackPressed() {
-        //Back Btn do nothing !
-//        super.onBackPressed();
     }
 }

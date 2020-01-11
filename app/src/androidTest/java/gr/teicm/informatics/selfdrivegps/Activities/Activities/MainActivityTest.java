a378caf7da14cf8ca68f92d1e198d7871184b887
Dimitris Dimitriadis <dimitris.dimtriadis@gmail.com>
2020-01-11 23:00:47 +0200

    sync with mac

---
+++ app/src/androidTest/java/gr/teicm/informatics/selfdrivegps/Activities/Activities/MainActivityTest.java
@@ -0,0 +1,27 @@
+package gr.teicm.informatics.selfdrivegps.Activities.Activities;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import gr.teicm.informatics.selfdrivegps.Activities.MainActivity;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void listGoesOverTheFold() {
+        onView(withText("Hello world!")).check(matches(isDisplayed()));
+    }
+
+}
--- app/src/test/java/gr/teicm/informatics/selfdrivegps/ExampleUnitTest.java
+++ app/src/test/java/gr/teicm/informatics/selfdrivegps/ExampleUnitTest.java
@@ -1,6 +1,5 @@
 package gr.teicm.informatics.selfdrivegps;
 
-import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.*;
 
@@ -10,8 +9,8 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
+//    @Test
+//    public void addition_isCorrect() throws Exception {
+//        assertEquals(4, 2 + 2);
+//    }
 }

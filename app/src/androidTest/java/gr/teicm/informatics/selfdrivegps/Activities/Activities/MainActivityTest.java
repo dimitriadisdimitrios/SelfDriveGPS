70c2ca961691d214eae29084f391dd8f13290d7f
dDimitriadis <dimitris.dimtriadis@gmail.com>
2020-01-19 15:15:44 +0200

    testing v0.01 first init

--- app/src/androidTest/java/gr/teicm/informatics/selfdrivegps/Activities/Activities/MainActivityTest.java
+++ app/src/androidTest/java/gr/teicm/informatics/selfdrivegps/Activities/Activities/MainActivityTest.java
@@ -1,27 +1,48 @@
 package gr.teicm.informatics.selfdrivegps.Activities.Activities;
 
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.rule.ActivityTestRule;
-
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.rule.ActivityTestRule;
+
 import gr.teicm.informatics.selfdrivegps.Activities.MainActivity;
+import gr.teicm.informatics.selfdrivegps.R;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4ClassRunner.class)
 public class MainActivityTest {
+    private String stringToBetyped;
+
     @Rule
-    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+    public ActivityTestRule<MainActivity> activityRule
+            = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void initValidString() {
+        // Specify a valid string.
+        stringToBetyped = "Espresso";
+    }
 
     @Test
-    public void listGoesOverTheFold() {
-        onView(withText("Hello world!")).check(matches(isDisplayed()));
+    public void changeText_sameActivity() {
+        // Type text and then press the button.
+        onView(withId(R.id.editTextUserInput))
+                .perform(typeText(stringToBetyped), closeSoftKeyboard());
+        onView(withId(R.id.changeTextBt)).perform(click());
+
+        // Check that the text was changed.
+        onView(withId(R.id.textToBeChanged))
+                .check(matches(withText(stringToBetyped)));
     }
 
 }
